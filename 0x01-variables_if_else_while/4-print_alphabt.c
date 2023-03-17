207c6a419ae9aafe85a4d73b4b493e16f53104ed
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-17 16:48:26 -0700

    all done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int i;
+
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        if (i != 'e' && i != 'q')
+        {
+            putchar(i);
+        }
+    }
+
+    putchar('\n');
+
+    return (0);
+}
+
