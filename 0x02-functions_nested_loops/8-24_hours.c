1aa1f64c8564f654725ba5c7d9596d1e180bde36
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-21 16:03:44 -0700

    fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -17,12 +17,13 @@ void jack_bauer(void)
 		{
 			if (h2 == '2' || h1 == '4')
 				break;
-			for (m2 = '0'; m2 < '7'; m2++)
+			for (m2 = '0'; m2 < '6'; m2++)
 			{
 				for (m1 = '0'; m1 <= '9'; m1++)
 				{
 					_putchar(h2);
 					_putchar(h1);
+					_putchar(':');
 					_putchar(m2);
 					_putchar(m1);
 					_putchar('\n');
