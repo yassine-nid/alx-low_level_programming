c5b40bdb7f181ecc1217a04cc02688b27967638e
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-23 18:36:45 -0700

    fix 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,48 +1,37 @@
 #include <stdio.h>
 #include "main.h"
-/**
-* print_remaining_days - takes a date and prints how many days are
-* left in the year, taking leap years into account
-* @month: month in number format
-* @day: day of month
-* @year: year
-* Return: void
-*/
 
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
 void print_remaining_days(int month, int day, int year)
 {
-    int days_in_month[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-    int day_count = day;
-    int i;
-
-    for (i = 0; i < month - 1; i++)
-    {
-        day_count += days_in_month[i];
-    }
-
-    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
     {
         if (month > 2)
         {
-            day_count++;
+            day++;
         }
 
-        if (day > 29)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-            return;
-        }
+        printf("Day of the year: %d\n", day);
+        printf("Remaining days: %d\n", 366 - day);
     }
     else
     {
-        if (day > days_in_month[month - 1])
+        if (month == 2 && day == 60)
         {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
-            return;
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
         }
     }
-
-    printf("Day of the year: %d\n", day_count);
-    printf("Remaining days: %d\n", (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0)) ? 366 - day_count : 365 - day_count);
 }
 
