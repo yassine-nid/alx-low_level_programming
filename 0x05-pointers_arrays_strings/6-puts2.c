1382603d02e1a03950d300e6ada32cdeb1763523
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-27 18:43:20 -0700

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,7 +7,7 @@
 
 void puts2(char *str)
 {
-	while (*str != '\0')
+	while (*str != '\0' && *(str - 1) != '\0')
 	{
 		_putchar(*str);
 		str++;
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts_half-prints half of a string
+ * @str: pointer to string
+ */
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int i;
+
+	while (*(str + l) != '\0')
+	{
+		l++;
+	}
+	for (i = l / 2; i < l; i++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
