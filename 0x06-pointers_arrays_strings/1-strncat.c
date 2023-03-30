498c1f081053808f494c29c4de61e126d04fc142
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-30 09:03:43 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strncat-concatenates two strings
+ * @dest: distination
+ * @src: source
+ * @n: number of bites
+ *
+ * Return: poiter to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p;
+	
+	p = dest;
+	while (*p != '\0')
+		p++;
+
+	while (*src != '\0' && n > 0)
+	{
+		p = src;
+		p++;
+		src++;
+		n--;
+	}
+	return (dest);
+}
