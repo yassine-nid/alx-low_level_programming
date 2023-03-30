4a482a694418858118f31f67d3b085d59556528e
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-30 10:30:38 -0700

    fix 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,28 +9,25 @@
 char *leet(char *str)
 {
     int i, j;
-    char leet_map[256] = {0};
-
-    /* populate the leet_map array with the encoding rules */
-    leet_map['a'] = '4';
-    leet_map['A'] = '4';
-    leet_map['e'] = '3';
-    leet_map['E'] = '3';
-    leet_map['o'] = '0';
-    leet_map['O'] = '0';
-    leet_map['t'] = '7';
-    leet_map['T'] = '7';
-    leet_map['l'] = '1';
-    leet_map['L'] = '1';
+    char *letters = "aeotl";
+    char *numbers = "43071";
 
     /* iterate through each character in str */
     for (i = 0; str[i] != '\0'; i++)
     {
-        /* check if the character should be encoded */
-        if (leet_map[str[i]] != 0)
-            str[i] = leet_map[str[i]];
+        /* iterate through each letter in letters */
+        for (j = 0; letters[j] != '\0'; j++)
+        {
+            /* if the current character in str matches a letter in letters */
+            if (str[i] == letters[j] || str[i] == letters[j] - 32)
+            {
+                /* replace the current character with the corresponding number */
+                str[i] = numbers[j];
+            }
+        }
     }
 
     return str;
 }
+
 
