85b43bb23012d3f5204d32b3bd796d31e1ae8ac2
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-30 08:38:35 -0700

    0.strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat-concatenates two strings
+ * @dest:the return value
+ * @src: the source string
+ *
+ * Return: concatenates string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *p;
+
+	p = dest;
+	while(*p != '\0')
+	{
+		p++;
+	}
+
+	while(*src != '\0')
+	{
+		*p = *src;
+		p++;
+		src++;
+	}
+	return (p);
+}
