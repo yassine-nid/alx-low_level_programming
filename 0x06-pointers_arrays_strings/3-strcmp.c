96ceb30b969a4757cafb5709e3220bb27d09815b
yassine-nid <nidkouchiyassine@gmail.com>
2023-03-30 10:12:36 -0700

    fix 3 2

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,28 +3,20 @@
 /**
  * _strcmp - compares two strings
  * @s1: the first string to compare
- * @s2: the second string to comapre
+ * @s2: the second string to compare
  *
- * Return: -1 if s1 is less than s2 zero if they are equal 1 if s1 is greater
+ * Return: an integer less than, equal to, or greater than zero if s1 is found,
+ * respectively, to be less than, to match, or be greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+    int i = 0;
 
-	for (i = 0; s1[i] == '\0' || s2[i] == '\0'; i++)
-	{
-		if (s1[i] < s2[i])
-			return (-1);
-		else if (s1[i] > s2[i])
-			return (1);
-	}
+    /* compare corresponding characters of s1 and s2 */
+    while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
+        i++;
 
-	if (s1[i] == '\0' && s2[i] == '\0')
-		return (0);
-
-	else if (s1[i] == '\0')
-		return (-1);
-
-	else return (1);
+    /* return the difference between the last compared characters */
+    return s1[i] - s2[i];
 }
+
