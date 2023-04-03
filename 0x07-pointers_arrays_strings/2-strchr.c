e03f7f08933665e7d6e497004050b5f72d4db425
yassine-nid <nidkouchiyassine@gmail.com>
2023-04-03 15:02:31 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr- locates a character in a string
+ * @s: pointer to string
+ * @c: character to find
+ *
+ * Return: pointer to char or null if the char doesn't exist
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	return (NULL);
+
