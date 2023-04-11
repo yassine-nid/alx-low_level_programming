571a8b6bc105821e9bc03edbce7e7e7dd8036488
yassine-nid <nidkouchiyassine@gmail.com>
2023-04-10 19:31:43 -0700

    fix 1 0

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# Compile all .c files in the current directory into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create the static library
+gcc -c *.c
 ar rcs liball.a *.o
-
-# Clean up the object files
-rm *.o
 
