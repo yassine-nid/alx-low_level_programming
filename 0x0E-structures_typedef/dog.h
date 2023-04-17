a43cda5abada098f54cb826f1a1f4d8c81c10de8
yassine-nid <nidkouchiyassine@gmail.com>
2023-04-17 16:48:44 -0700

    #0x0E. C - Structures, typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * struct dog - dog struct
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owhner of the dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dog.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
