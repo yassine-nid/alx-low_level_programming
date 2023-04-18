f99eb2edbf7f4bf7f6969ea7278a890d58b27835
yassine-nid <nidkouchiyassine@gmail.com>
2023-04-17 17:24:34 -0700

    fix 0 agin

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,19 @@
+/*struct dog - struct of a dog
+ * @name: the dog's name
+ * @age: the age of the dog
+ * @owner: the owner's name
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct
+ */
+
 typedef struct dog dog_t;
 
